f7b7dbe368b622da60183f88a0682fdbd2910612
Abdul Charis <konfoy7@gmail.com>
2025-01-14 19:25:51 +0700

    membenarkan syntax yang salah dan debugger

--- carosel image/script.js
+++ carosel image/script.js
@@ -1,61 +1,61 @@
-//get element from HTML 
-//container for all image
-const crouselSlide = document.querySelector('.crousel-slide');
-const crouselImage = document.querySelectorAll('.crousel-slide img');
+//get element HTMl
+//get container for all image
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImage = document.querySelectorAll('.carousel-slide img');
+
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
+
 const indicators = document.querySelectorAll('.indicator');
 
-//state for curent index
+//state current index
 let currentIndex = 0;
-const totalImage = crouselImage.length;
+const totalImage = carouselImage.length;
 
-//variabel to save interval auto slide
+//variabel to save internal slide
 let autoSlideInterval;
-console.log(crouselImage)
-//function to udate crousel
-function updateCrousel(){
-    crouselSlide.style.transfrom =`translateX(${-currentIndex * 100 } %)`
 
-    //udate indikator active
+function updateCarousel(){
+    carouselSlide.style.transform = `translateX(${-currentIndex * 100 }%)`;
+
+    //update indikator active
     indicators.forEach((indicators, index) => {
-        indicators.classList.toggle('active', index === currentIndex)
+        indicators.classList.toggle('active', index === currentIndex);
     })
-
-    
 }
-//function to next slide 
-function nextSlide() {
-    currentIndex = (currentIndex + 1) % totalImage
-    updateCrousel()
-    resetAutoSlide()
-  console.log(console.log(`Current Index: ${currentIndex}`))
-};
 
-//function to prev slide
-function prevSlide() {
-    currentIndex = (currentIndex - 1 + totalImage) % totalImage
-    updateCrousel()
+function nextSlide(){
+    currentIndex = (currentIndex + 1) % totalImage;
+    updateCarousel()
     resetAutoSlide()
-};
+}
 
-//function to reset auto slide
-function resetAutoSlide() {
-    clearInterval(autoSlideInterval)
+
+function prevSlide(){
+    currentIndex = (currentIndex - 1 + totalImage) % totalImage;
+    updateCarousel()
+    resetAutoSlide()
+}
+
+function resetAutoSlide(){
+    clearInterval(autoSlideInterval);
+
     autoSlideInterval = setInterval(nextSlide, 5000)
-};
+
+}
 
 
-//add eventlistener to next,prev and indicator
-nextBtn.addEventListener('click', nextSlide);
-prevBtn.addEventListener('click', prevSlide);
+//addeventlistener for next
+nextBtn.addEventListener('click', nextSlide)
 
+//addeventlistener for prev
+prevBtn.addEventListener('click', prevSlide)
+
+//addeventlistener for indicator
 indicators.forEach((indicators, index) => {
-    indicators.addEventListener('click', () => {
-        currentIndex = index
-        updateCrousel()
-        resetAutoSlide()
-    })
+indicators.addEventListener('click', () =>{
+    currentIndex = index
+    updateCarousel()
+    resetAutoSlide()
 })
-
-resetAutoSlide();
+})
